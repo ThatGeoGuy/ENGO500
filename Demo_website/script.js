bd4af22e92d45f3ab9e05438ebf19c61126e422e
b-en <btrodd@gmail.com>
2013-12-01 00:41:54 -0700

    Added some code for a demo website

---
+++ Demo_website/script.js
@@ -0,0 +1,44 @@
+// Definitions
+var rootURI = 'http://demo.student.geocens.ca:8080/SensorThings_V1.0/';
+
+// callback in a parameter that represents a function to be executed whenever the data is loaded successfully
+function getData(path, callback) {
+    $.getJSON(path, callback);
+}
+
+// Get root directory information
+var root;
+var things;
+var dataStreams;
+
+root = getData(rootURI, function(jsonData) {
+    root = jsonData;
+	console.log(root);
+	
+	// Get things on database, display to screen
+	things = getData(root.Collections[0].Things.uri, function(jsonData) {
+		things = jsonData;
+		console.log(things);
+		$('#thingsLevel').append('<div id=thingElement>' + things.Things[0].Description + '</div>');
+		
+		// Get dataStreams for things
+		dataStreams = getData( rootURI + things.Things[0].Datastreams["Navigation-Link"], function(jsonData) {
+			dataStreams = jsonData;
+			console.log(dataStreams);
+			// If a thing is clicked, load it's datastreams
+			$('#thingElement').click(function() {
+					for(var i=0; i< dataStreams.Datastreams.length; i++){
+						$('#dataStreamsLevel').append('<div id=dataStreamElement>' + i + ' ' + dataStreams.Datastreams[i].Description + '</div>');
+						
+					}	
+			});
+
+			return dataStreams
+		});
+		return things;
+	});
+	return root;
+});
+
+
+console.log("CHEESE");
