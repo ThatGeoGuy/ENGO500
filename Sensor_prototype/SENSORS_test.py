03590dabb5b66a1da1ba830086b91953c9240c24
acummins <alex13@shaw.ca>
2014-03-06 14:14:00 -0700

    Added code to manage all sensors

--- Sensor_prototype/SENSORS_test.py
+++ Sensor_prototype/SENSORS_test.py
@@ -1,57 +1,79 @@
-# Detect motion from PIR module and possibly blink LED
-# by AC Jan 30 2014
+# Detect motion from PIR module and PhotoInterrupterSwitch
+# AC march 6 2014
 
 # Import required Python libraries
 import RPi.GPIO as GPIO, time, datetime
-#import testclass_olab3.py
 
 # Use BCM GPIO references instead of physical pin numbers
 GPIO.setmode(GPIO.BCM)
-# Define GPIO to use on Pi (11 for now. gpio 0)
+# Define GPIO to use on Pi
 GPIO_PIR = 18
+GPIO_Switch = 2
+
 # Set pin as input, check connection to sensor
 GPIO.setup(GPIO_PIR,GPIO.IN)
 Current_State  = 0
 Previous_State = 0
+Switch_State = 0
+
+def PhotoIntSwitch(PiPin1):
   
-def detectmov(PiPin):
+  Switch_State = GPIO.input(GPIO_Switch)
+  while True:
+    if Switch_State == 0
+        continue
+    else
+      break
+return Switch_State
+  
+
+#---------This whole function will need to reworked with 'data' class----------#
+def detectmov(PiPin):  
   
   print "Waiting for PIR to settle ..." 
   # This will run unless PIR intput is zero, ensuring that it settles
   while GPIO.input(GPIO_PIR)==1:
     Current_State  = 0
   print "  Ready"
+  
   # Loop until users quits with CTRL-C
   while True :
+    
     # Read PIR state
     Current_State = GPIO.input(GPIO_PIR)
     if Current_State==1: #and Previous_State==0:
+      
     # PIR is triggered
       print "  Motion detected!"
-      #print time.localtime(time.time())
       print datetime.datetime.now()
-      #records most recent time
+      
+      # Records most recent time
       timerec = datetime.datetime.now()
-        # Voltage is reocrded as high
-        # voltage.v  = high
-        # ACTUAL VALUE
-      # GPIO.output(7,True) - LED on
+        # GPIO.output(7,True) - LED on
+      
       # Update previous state
       Previous_State=1
+      
     elif Current_State==0 and Previous_State==1:
+      
       # PIR has returned to ready state
-      # GPIO.output(7, False) - LED off
+        # GPIO.output(7, False) - LED off
       print "  Ready"
       Previous_State=0
+      
     # Wait for 10 milliseconds; may change with testing
     time.sleep(0.01)
   return timerec
+
+
 
     
 #Main Program Loop
 t = []
 try:
-  t.append(detectmov(PiPin))
+  #t.append(detectmov(PiPin))
+  SwitchTest = PhotoIntSwitch(PiPin2)
+  print (SwitchTest)
    
 except KeyboardInterrupt:
   print "  Quit"
