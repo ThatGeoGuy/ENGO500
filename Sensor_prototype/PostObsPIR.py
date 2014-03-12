aa6dd2c5ef3514ac866139bb65edfa0295b02203
acummins <alex13@shaw.ca>
2014-03-12 11:05:31 -0600

    Added 'with' statements and LED condition
    -Now uses 'with' statement to ensure files are closed after use
    -LED now illuminates if object is 404 because of server reset

--- Sensor_prototype/PostObsPIR.py
+++ Sensor_prototype/PostObsPIR.py
@@ -2,10 +2,10 @@
 # PostObsPIR.py
 # an attempt at mashing up existing scripts (oh boy!)
 # and cleaning them up while we're at it
-# Last Edited: march 8 2014 AC
+# Last Edited: march 12 2014 AC
 
-ctl=open('/home/pi/ENGO500/Sensor_prototype/checkthinglocation.txt','a+')
-csl=open('/home/pi/ENGO500/Sensor_prototype/checksensorlocation.txt','a+')
+#ctl=open('/home/pi/ENGO500/Sensor_prototype/checkthinglocation.txt','a+')
+#csl=open('/home/pi/ENGO500/Sensor_prototype/checksensorlocation.txt','a+')
 
 
 import time, datetime, requests, json, RPi.GPIO as GPIO
@@ -24,62 +24,64 @@ class data:
 
     def initRequest(self):
         n=1
-        c = ctl.read(n)
-        if (c == 'h'):
-            print("Things exists")
-            thing_location = ctl.readline()
-            self.thing_location = 'h' + thing_location
-            print(self.thing_location)
-            r2 = requests.get(self.thing_location)
-            sr2 = str(r2)
-            print(sr2)
-            print('check r2')
-            if (sr2 == '<Response [404]>'):
-                ctl.seek(0)
-                ctl.truncate()
-                csl.seek(0)
-                csl.truncate()
-                print(sr2)
-                print("Server has reset. Links have been changed, please restart.")
-                exit()
-            elif (sr2 == '<Response [200]>'):
-                print(sr2)
-                print('this is 200')
-            else:
-                print("rcode sting match failed")
-            self.sensors_location = csl.readline()
-            #print(self.sensors_location)
-            rcode = requests.get(self.sensors_location)
-            response = rcode.json()
-            self.sensorID = response['ID']
-            print(self.sensorID)   
-            
-        else:
-            print("Thing does not exist.  Create thing.")
-            url = self.rootURI() + 'Things'
-            payload = {'Description': 'This is the real TurboCat (Raspberry Pi)',
-                'Datastreams':
-                    [{'Description': 'This is a datastream for measuring people traffic'},
-                        {'Description': 'This is a second datastream'}]}
-            headers = {'content-type': 'application/json'}
+        with open('/home/pi/ENGO500/Sensor_prototype/checkthinglocation.txt', 'a+') as ctl:
+            with open('/home/pi/ENGO500/Sensor_prototype/checksensorlocation.txt','a+') as csl:
+                c = ctl.read(n)
+                if (c == 'h'):
+                    print("Things exists")
+                    thing_location = ctl.readline()
+                    self.thing_location = 'h' + thing_location
+                    print(self.thing_location)
+                    r2 = requests.get(self.thing_location)
+                    sr2 = str(r2)
+                    print(sr2)
+                    print('check r2')
+                    if (sr2 == '<Response [404]>'):
+                        ctl.seek(0)
+                        ctl.truncate()
+                        csl.seek(0)
+                        csl.truncate()
+                        print(sr2)
+                        print("Server has reset. Links have been changed, please restart.")
+                        #LED HOLD
+                        GPIO.output(GPIO_LED, True)
+                        exit()
+                    elif (sr2 == '<Response [200]>'):
+                        print(sr2)
+                        print('this is 200')
+                    else:
+                        print("rcode sting match failed")
+                    self.sensors_location = csl.readline()
+                    rcode = requests.get(self.sensors_location)
+                    response = rcode.json()
+                    self.sensorID = response['ID']
+                    print(self.sensorID)   
+                    
+                else:
+                    print("Thing does not exist.  Create thing.")
+                    url = self.rootURI() + 'Things'
+                    payload = {'Description': 'This is the real TurboCat (Raspberry Pi)',
+                        'Datastreams':
+                            [{'Description': 'This is a datastream for measuring people traffic'},
+                                {'Description': 'This is a second datastream'}]}
+                    headers = {'content-type': 'application/json'}
 
-            r = requests.post(url, data=json.dumps(payload), headers=headers)
-            ## ERROR CHECKING?! status code
-            thing_location = r.headers['location']
-            self.thing_location = thing_location
-            ctl.write(thing_location)
+                    r = requests.post(url, data=json.dumps(payload), headers=headers)
+                    ## ERROR CHECKING?! status code
+                    thing_location = r.headers['location']
+                    self.thing_location = thing_location
+                    ctl.write(thing_location)
 
-            url3 = self.rootURI() + 'Sensors'
-            payload2 = {'Metadata': 'This is a PIR sensor'}
-            rsensor = requests.post(url3, data=json.dumps(payload2), headers=headers)
-            self.sensors_location = rsensor.headers['location']
+                    url3 = self.rootURI() + 'Sensors'
+                    payload2 = {'Metadata': 'This is a PIR sensor'}
+                    rsensor = requests.post(url3, data=json.dumps(payload2), headers=headers)
+                    self.sensors_location = rsensor.headers['location']
 
-            csl.write(self.sensors_location)
-            rsensor = requests.get(self.sensors_location)
-            response = rsensor.json()
-            self.sensorID = response['ID']            
+                    csl.write(self.sensors_location)
+                    rsensor = requests.get(self.sensors_location)
+                    response = rsensor.json()
+                    self.sensorID = response['ID']            
 
-        #check txt files
         return thing_location
 
     def sendObs(self, obs):
